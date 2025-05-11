39d7633763d6a4c47c398132e357812d25a086ea
Kunal Sharma <kunalsh5739@gmail.com>
2025-05-11 15:08:49 +0530

    Add files via upload

---
+++ player/camera.gd
@@ -0,0 +1,12 @@
+extends Node3D
+
+var senstivity = 0.2
+
+func _ready() -> void:
+	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+
+func _input(event: InputEvent) -> void:
+	if event is InputEventMouseMotion:
+		get_parent().rotate_y(deg_to_rad(-event.relative.x * senstivity))
+		rotate_x(deg_to_rad(-event.relative.y * senstivity))
+		rotation.x = clamp(rotation.x, deg_to_rad(-90), deg_to_rad(90))
